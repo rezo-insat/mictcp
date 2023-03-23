326e791b8e4dd84fe9e463521a3f2e175a476281
jules-ian <115194459+jules-ian@users.noreply.github.com>
2023-03-23 19:10:13 +0100

    Adding protocol_state values ACCEPTING and WAITING

--- include/mictcp.h
+++ include/mictcp.h
@@ -21,7 +21,7 @@
  */
 typedef enum protocol_state
 {
-    IDLE, CLOSED, SYN_SENT, SYN_RECEIVED, ESTABLISHED, CLOSING
+    IDLE, CLOSED, SYN_SENT, SYN_RECEIVED, ESTABLISHED, CLOSING, ACCEPTING, WAITING
 } protocol_state;
 
 /*
